e5806ef9fa88322aa1c5c4ae3b456a73a8465069
SiddharthSajeev <siddharthsajeev14@gmail.com>
2021-06-01 05:42:36 +0530

    Estimate.py has been updated

--- estimate.py
+++ estimate.py
@@ -1,32 +1,27 @@
-import math
-import unittest
-
-class TestWallis(unittest.TestCase):
-    def test_low_iters(self):
-        for i in range(0, 5):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) > 0.15, msg=f"Estimate with just {i} iterations is {pi} which is too accurate.\n")
-            
-    def test_high_iters(self):
-        for i in range(500, 600):
-            pi = wallis(i)
-            self.assertTrue(abs(pi - math.pi) < 0.01, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-
-
-class TestMC(unittest.TestCase):
-    def test_randomness(self):
-        pi0 = monte_carlo(15000)
-        pi1 = monte_carlo(15000)
-        
-        self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
-
-        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
-
-    def test_accuracy(self):
-        for i in range(500, 600):
-            pi = monte_carlo(i)
-            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
-        
-    
-if __name__ == "__main__":
-    unittest.main()
+import random
+n=int(input("Enter numer of iterations"));
+def wallis():
+ global n
+ incircle=0
+ outcircle=0
+ for n in range(n):
+  x=random.random()
+  y=random.random()
+  if(x>y):
+       if(y>=((1-x**2)**0.5)):
+        incircle+=1
+       else:
+        outcircle+=1
+  elif(y>x):
+       if(y>=((1-x**2)**0.5)):
+        incircle+=1
+       else:
+        outcircle+=1
+  elif(x==y): 
+       if(x>(1/(2**0.5))):
+        outcircle+=1
+       else:
+        incircle+=1
+ pi=16*(incircle/(outcircle+incircle))
+ print("The estimated value of pi is",pi);
+wallis();
